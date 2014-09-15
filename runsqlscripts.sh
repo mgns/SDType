e9b2a8ab1d5dd14a5b710eccdf1219e55753177e
mgns <magnus@kashyyyk.yovisto.com>
2014-09-15 16:17:15 +0200

    fixed replace of dollar sign

--- runsqlscripts.sh
+++ runsqlscripts.sh
@@ -1,6 +1,5 @@
 
-for languageprefix in 'en_' 'ca_' 'de_' 'es_' 'eu_' 'fr_' 'id_' 'it_' 'ja_' 'ko_' 'nl_' 'pl_' 'pt_' 'ru_' 'tr_'
+for languageprefix in 'en' 'ca' 'de' 'es' 'eu' 'fr' 'id' 'it' 'ja' 'ko' 'nl' 'pl' 'pt' 'ru' 'tr'
 do
-	cat generate_types.sql | sed "s/prefix_/$languageprefix/g" | mysql -u dstype -p dstype &
-
+	cat generate_types.sql | sed -E "s/[$]prefix[$]/$languageprefix/g" | mysql -u dstype -p dstype &
 done
